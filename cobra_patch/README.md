61cc8f1b0a05bc554a7d85a58ec96a69779ac8ec
Fxe <fliu@anl.gov>
2021-03-04 00:49:20 -0600

    fixed 256 char length limit

---
+++ cobra_patch/README.md
@@ -0,0 +1,1 @@
+patches cobrapy Model/Solver to ignore solver calls, KBase module only supports GLPK that limits 256 char variables thus breaks when export models with long identifiers
---
+++ cobra_patch/test.py
@@ -0,0 +1,29 @@
+import cobra
+import optlang
+import cobrakbase
+# old method
+from cobra.core import Model
+from cobrakbase.core.model import KBaseFBAModel
+from cobrakbase.core import KBaseGenome
+from cobrakbase.core.converters import KBaseFBAModelToCobraBuilder
+
+kbase = cobrakbase.KBaseAPI('PLCZDTFJ2IHGNYRCU6TUNR6GPQZHH6WM')
+
+model_object = kbase.get_object('MS1.mdl.gf', 'filipeliu:narrative_1612368108584')
+
+super_long_id = 'SUPER_LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOG'
+print(len(super_long_id))
+
+model_object['modelreactions'][0]['id'] = super_long_id
+
+fbamodel = KBaseFBAModel(model_object)
+builder = KBaseFBAModelToCobraBuilder(fbamodel)
+if 'genome_ref' in model_object and False:
+    #logging.info(f"Annotating model with genome information: {model_object['genome_ref']}")
+    genome_object = kbase.get_object('80971/3/1', None)
+    #adding Genome to the Builder
+    builder.with_genome(KBaseGenome(genome_object))
+
+model = builder.build()
+
+cobra.io.write_sbml_model(model, 'sbml.txt')
